b01cc6833e41fd0b1bcaab7de3ddd036e1d01938
Victor Garcia <victor.garcia@malt.com>
2024-12-10 16:06:54 +0100

    chore: added publish script

---
+++ scripts/publish.mjs
@@ -0,0 +1,18 @@
+#!/usr/bin/env zx
+
+import { prerelease } from 'semver';
+import { $ } from 'zx';
+const { version } = require('../package.json');
+
+if (process.env.NODE_AUTH_TOKEN) {
+  const tags = prerelease(version);
+  try {
+    if (tags?.length) {
+      await $`pnpm -r --filter='@regle/*' publish --report-summary --dry-run --no-git-checks --tag ${tags[0]}`;
+    } else {
+      await $`pnpm -r --filter='@regle/*' publish --report-summary --dry-run --no-git-checks`;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
--- tests/unit/useRegle/errors/metadata.spec.ts
+++ tests/unit/useRegle/errors/metadata.spec.ts
@@ -1,5 +1,56 @@
+import { createRule, useRegle } from '@regle/core';
+import { withMessage, withParams } from '@regle/rules';
+import { ref } from 'vue';
+
+function metadateRules() {
+  const externalDep = ref('external');
+  const inlineMetadataRule = withMessage(
+    withParams((value, external) => ({ $valid: false, external, value }), [externalDep]),
+    (_, { external, value }) => `${external}-${value}`
+  );
+  const createRuleMetadataRule = createRule({
+    validator: (value, external) => ({ $valid: false, external, value }),
+    message: (_, { external, value }) => `${external}-${value}`,
+  });
+
+  const form = ref({
+    email: '',
+    user: {
+      firstName: '',
+      nested: {
+        child: '',
+        collection: [{ name: '' }],
+      },
+    },
+    contacts: [{ name: '' }],
+  });
+
+  const allRules = { inlineMetadataRule, createRuleMetadataRule: createRuleMetadataRule(externalDep) };
+
+  return useRegle(form, {
+    email: allRules,
+    user: {
+      firstName: allRules,
+      nested: {
+        child: allRules,
+        collection: {
+          ...allRules,
+          $each: {
+            name: allRules,
+          },
+        },
+      },
+    },
+    contacts: {
+      $each: {
+        name: allRules,
+      },
+    },
+  });
+}
+
 describe('metadata', () => {
-  it('should work', () => {
+  it('correctly return metadata for validator and active handlers', () => {
     expect(true).toBe(true);
   });
 });
