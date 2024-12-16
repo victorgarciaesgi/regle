2755c1c83555a071b2828007b01c6067eb2612ce
Victor Garcia <victor.garcia@malt.com>
2024-12-16 14:08:50 +0100

    docs: updated favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -166,6 +166,7 @@ export default defineConfig({
     },
   },
   head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' }],
     ['meta', { name: 'theme-color', content: '#00bb7f' }],
     [
