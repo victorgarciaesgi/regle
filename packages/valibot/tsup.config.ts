349b354192fd392bd56cd575976af085e3afc71f
Victor Garcia <victor.garcia@malt.com>
2024-12-20 19:02:40 +0100

    fixed env build

--- packages/valibot/tsup.config.ts
+++ packages/valibot/tsup.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'tsup';
-import { defaultOptions } from '../../tsup.common.build';
+import { defineConfig, type Options } from 'tsup';
+import { defaultOptions, outExtension } from '../../tsup.common.build';
 
-export default defineConfig({
+const sharedOptions: Options = {
   ...defaultOptions,
+  entry: { 'regle-valibot': 'src/index.ts' },
   external: ['vue', 'valibot'],
-});
+};
+
+export default defineConfig([
+  {
+    ...sharedOptions,
+    outExtension: outExtension(),
+  },
+  {
+    ...sharedOptions,
+    minify: true,
+    outExtension: outExtension(true),
+  },
+]);
--- packages/valibot/tsup.sourcemap.ts
+++ packages/valibot/tsup.sourcemap.ts
@@ -4,14 +4,18 @@ import { defaultOptions, outExtension } from '../../tsup.common.build';
 const sharedOptions: Options = {
   ...defaultOptions,
   entry: { 'regle-valibot': 'src/index.ts' },
-  dts: true,
-  clean: false,
   external: ['vue', 'valibot'],
+  sourcemap: 'inline',
 };
 
 export default defineConfig([
   {
     ...sharedOptions,
     outExtension: outExtension(),
+  },
+  {
+    ...sharedOptions,
+    minify: true,
+    outExtension: outExtension(true),
   },
 ]);
