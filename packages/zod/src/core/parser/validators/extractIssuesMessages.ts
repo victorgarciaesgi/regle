fb7baeea814ecb0743c51ceb043a0d59a5f00165
Felix Schlitter <felixschlitter@gmail.com>
2025-01-19 20:43:52 +1300

    Fix potential access on undefined

--- packages/zod/src/core/parser/validators/extractIssuesMessages.ts
+++ packages/zod/src/core/parser/validators/extractIssuesMessages.ts
@@ -4,6 +4,6 @@ import type { ZodIssue } from 'zod';
 export function extractIssuesMessages() {
   return ((metadata: { $issues: ZodIssue[] }) => {
     const issueMessages = metadata.$issues?.map((issue) => issue.message);
-    return issueMessages.length ? issueMessages : 'Error';
+    return issueMessages?.length ? issueMessages : 'Error';
   }) satisfies RegleRuleDefinitionWithMetadataProcessor<any, any, any>;
 }
