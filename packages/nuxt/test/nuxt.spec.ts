59f047e704e9ac069ee2d1a19cf5f7cd596d6303
Victor Garcia <victor.garcia@malt.com>
2025-03-25 18:40:18 +0100

    chore: ptin de flacky test nuxt

--- packages/nuxt/test/nuxt.spec.ts
+++ packages/nuxt/test/nuxt.spec.ts
@@ -1,13 +1,14 @@
-import { describe, it, expect, beforeAll } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { fileURLToPath } from 'node:url';
-import { setup, $fetch, createPage } from '@nuxt/test-utils/e2e';
+import { setup, createPage } from '@nuxt/test-utils/e2e';
 import { expectNoClientErrors } from './utils/error.utils';
 
 describe('Nuxt SSR', async () => {
   await setup({
     rootDir: fileURLToPath(new URL('./fixtures', import.meta.url)),
-    dev: true,
+    port: 7777,
     browser: true,
+    dev: true,
   });
 
   it('renders the index page', async () => {
