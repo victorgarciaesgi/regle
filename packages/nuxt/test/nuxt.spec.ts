d95f533927aacdd3f76c1d17d9d3df5c465c305e
Victor Garcia <victor.garcia@malt.com>
2025-03-25 18:45:19 +0100

    Revert "chore: ptin de flacky test nuxt"
    This reverts commit 59f047e704e9ac069ee2d1a19cf5f7cd596d6303.

--- packages/nuxt/test/nuxt.spec.ts
+++ packages/nuxt/test/nuxt.spec.ts
@@ -1,14 +1,13 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeAll } from 'vitest';
 import { fileURLToPath } from 'node:url';
-import { setup, createPage } from '@nuxt/test-utils/e2e';
+import { setup, $fetch, createPage } from '@nuxt/test-utils/e2e';
 import { expectNoClientErrors } from './utils/error.utils';
 
 describe('Nuxt SSR', async () => {
   await setup({
     rootDir: fileURLToPath(new URL('./fixtures', import.meta.url)),
-    port: 7777,
-    browser: true,
     dev: true,
+    browser: true,
   });
 
   it('renders the index page', async () => {
