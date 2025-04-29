7c70a585e33eac0ece3d4f103055966c79e14c50
Victor Garcia <victor.garcia@malt.com>
2025-04-29 10:01:50 +0200

    chore: test

--- playground/vue3/src/components/Test13.vue
+++ playground/vue3/src/components/Test13.vue
@@ -3,25 +3,26 @@ import { createVariant, defineRules, narrowVariant, refineRules, useRegle, type 
 import { email, literal, minLength, minValue, numeric, required, sameAs, type } from '@regle/rules';
 import { computed, ref } from 'vue';
 
-const defaultRules = defineRules({
-  firstName: { required },
-  password: { required, type: type<string>() },
-  type: { required, type: type<'ONE' | 'TWO'>() },
-});
+const rules = refineRules(
+  {
+    firstName: { required },
+    password: { required, type: type<string>() },
+    type: { required, type: type<'ONE' | 'TWO'>() },
+  },
+  (state) => {
+    const variant = createVariant(state, 'type', [
+      { type: { literal: literal('TWO') }, twoValue: { numeric, required } },
+      { type: { literal: literal('ONE') }, oneValue: { numeric, required, minValue: minValue(4) } },
+    ]);
 
-const rules = refineRules(defaultRules, (state) => {
-  const variant = createVariant(state, 'type', [
-    { type: { literal: literal('TWO') }, twoValue: { numeric, required } },
-    { type: { literal: literal('ONE') }, oneValue: { numeric, required, minValue: minValue(4) } },
-  ]);
+    console.log(state);
 
-  console.log(state);
-
-  return {
-    confirmPassword: { required, type: type<string>(), sameAs: sameAs(() => state.value.password) },
-    ...variant.value,
-  };
-});
+    return {
+      confirmPassword: { required, type: type<string>(), sameAs: sameAs(() => state.value.password) },
+      ...variant.value,
+    };
+  }
+);
 
 const state = ref<InferInput<typeof rules>>({});
 
