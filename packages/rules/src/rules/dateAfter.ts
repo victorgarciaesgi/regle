ac5346d788a2de3f1f02d9fccfcd8ed687ceec92
Jeremy Worboys <jw@jeremyworboys.com>
2025-04-30 18:10:38 +1000

    feat: Add comparison options date rules (#109)
    BREAKING CHANGE: `dateAfter`, `dateBefore` and `dateBetween` now default to `allowEqual: true` to stay consistent with other rules

--- packages/rules/src/rules/dateAfter.ts
+++ packages/rules/src/rules/dateAfter.ts
@@ -1,5 +1,5 @@
 import { isFilled, isDate, toDate } from '../helpers';
-import type { Maybe, RegleRuleWithParamsDefinition } from '@regle/core';
+import type { Maybe, RegleRuleWithParamsDefinition, CommonComparisonOptions } from '@regle/core';
 import { createRule } from '@regle/core';
 import { formatLocaleDate } from '../utils/getLocale.util';
 
@@ -7,10 +7,11 @@ import { formatLocaleDate } from '../utils/getLocale.util';
  * Checks if the date is after the given parameter.
  *
  * @param after - the date to compare to
+ * @param options - comparison options
  */
 export const dateAfter: RegleRuleWithParamsDefinition<
   string | Date,
-  [after: Maybe<string | Date>],
+  [after: Maybe<string | Date>, options?: CommonComparisonOptions],
   false,
   | true
   | {
@@ -23,10 +24,17 @@ export const dateAfter: RegleRuleWithParamsDefinition<
     }
 > = createRule({
   type: 'dateAfter',
-  validator: (value: Maybe<Date | string>, after: Maybe<Date | string>) => {
+  validator: (
+    value: Maybe<Date | string>,
+    after: Maybe<Date | string>,
+    options?: CommonComparisonOptions
+  ) => {
+    const { allowEqual = true } = options ?? {};
     if (isFilled(value) && isFilled(after)) {
       if (isDate(value) && isDate(after)) {
-        const result = toDate(value).getTime() > toDate(after).getTime();
+        const result = allowEqual
+          ? toDate(value).getTime() >= toDate(after).getTime()
+          : toDate(value).getTime() > toDate(after).getTime();
         if (result) {
           return true;
         }
