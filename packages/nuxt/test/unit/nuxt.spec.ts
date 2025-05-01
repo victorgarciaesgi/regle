76f1132ff1d83305162756340aab66e013e618dc
Victor Garcia <victorgarciaparis13@gmail.com>
2025-05-01 13:54:37 +0200

    feat: `setupFile` option in Nuxt module #117

--- packages/nuxt/test/unit/nuxt.spec.ts
+++ packages/nuxt/test/unit/nuxt.spec.ts
@@ -1,13 +1,12 @@
 import { describe, it, expect } from 'vitest';
 import { fileURLToPath } from 'node:url';
 import { setup, createPage } from '@nuxt/test-utils/e2e';
-import { expectNoClientErrors } from './utils/error.utils';
+import { expectNoClientErrors } from '../utils/error.utils';
 
 describe('Nuxt SSR', async () => {
   await setup({
-    rootDir: fileURLToPath(new URL('./fixtures', import.meta.url)),
+    rootDir: fileURLToPath(new URL('../fixtures', import.meta.url)),
     dev: true,
-    browser: true,
   });
 
   it('renders the index page', async () => {
@@ -16,5 +15,10 @@ describe('Nuxt SSR', async () => {
     const page = await createPage('/');
 
     expect(await page.getByTestId('acceptTC-checkbox').isVisible()).toBe(true);
+
+    await page.getByTestId('field-fullName').fill('foo');
+    await page.getByTestId('field-fullName').fill('');
+
+    expect(await page.getByTestId('field-fullName-errors').textContent()).toBe(`Custom Nuxt error`);
   });
 });
