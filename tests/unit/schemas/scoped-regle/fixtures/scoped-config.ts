a3b04a1db0b927cca0e22fbd1861484ba57b12a4
Victor Garcia <victor.garcia@malt.com>
2025-05-02 12:26:31 +0200

    feat: support scoped validation in `@regle/schemas` #115

---
+++ tests/unit/schemas/scoped-regle/fixtures/scoped-config.ts
@@ -0,0 +1,4 @@
+import { createScopedUseRegleSchema } from '@regle/schemas';
+
+export const { useCollectScope: useScope1Validations, useScopedRegle: useScoped1Regle } = createScopedUseRegleSchema();
+export const { useCollectScope: useScope2Validations, useScopedRegle: useScoped2Regle } = createScopedUseRegleSchema();
