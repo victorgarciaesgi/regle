1b08fd3b9af0ca264f2806401126ca984d6c3fc1
Victor Garcia <victor.garcia@malt.com>
2025-05-02 12:26:31 +0200

    feat: support scoped validation in `@regle/schemas` #115

--- packages/schemas/src/types/options.types.ts
+++ packages/schemas/src/types/options.types.ts
@@ -1,4 +1,1 @@
-export type RegleSchemaMode = 'rules' | 'schema';
-export type isModeRules<T> = T extends 'rules' ? true : false;
-
 export type $InternalRegleResult = { valid: boolean; data: any };
