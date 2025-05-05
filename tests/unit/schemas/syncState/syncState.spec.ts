f82eaa180c98cb0613d489bebac64f7f49bdf058
Victor Garcia <victor.garcia@malt.com>
2025-05-05 08:57:36 +0200

    chore: fixed apply default values no existing state in regle/schemas

--- tests/unit/schemas/syncState/syncState.spec.ts
+++ tests/unit/schemas/syncState/syncState.spec.ts
@@ -5,20 +5,18 @@ import { zodRegleTransform } from './fixtures/zod.fixture';
 describe.each([['zod', zodRegleTransform]])('%s - syncState', (name, schemaRegle) => {
   it('should update state correctly on valid submit', async () => {
     const { vm } = createRegleComponent(() => schemaRegle({ syncState: { onValidate: true } }));
-    expect(vm.r$.$value).toStrictEqual({});
+    expect(vm.r$.$value).toStrictEqual({ noValidationValue: 'foo' });
 
     shouldBeInvalidField(vm.r$);
 
-    vm.r$.$value = {
-      noChange: 'foo',
-      withDefault: undefined as any,
-      withCatch: 0 as any,
-      withTransform: 'foo',
-    };
-
+    vm.r$.$value.noChange = 'foo';
+    vm.r$.$value.withDefault = undefined as any;
+    vm.r$.$value.withCatch = 0 as any;
+    vm.r$.$value.withTransform = 'foo';
     await vm.$nextTick();
 
     expect(vm.r$.$value).toStrictEqual({
+      noValidationValue: 'foo',
       noChange: 'foo',
       withDefault: undefined,
       withCatch: 0,
@@ -29,6 +27,7 @@ describe.each([['zod', zodRegleTransform]])('%s - syncState', (name, schemaRegle
     await vm.$nextTick();
 
     expect(vm.r$.$value).toStrictEqual({
+      noValidationValue: 'foo',
       noChange: 'foo',
       withDefault: 'default value',
       withCatch: 'catch',
@@ -38,20 +37,18 @@ describe.each([['zod', zodRegleTransform]])('%s - syncState', (name, schemaRegle
 
   it('should update state correctly on valid submit', async () => {
     const { vm } = createRegleComponent(() => schemaRegle({ syncState: { onUpdate: true } }));
-    expect(vm.r$.$value).toStrictEqual({});
+    expect(vm.r$.$value).toStrictEqual({ noValidationValue: 'foo' });
 
     shouldBeInvalidField(vm.r$);
 
-    vm.r$.$value = {
-      noChange: 'foo',
-      withDefault: undefined as any,
-      withCatch: 0 as any,
-      withTransform: 'foo',
-    };
-
+    vm.r$.$value.noChange = 'foo';
+    vm.r$.$value.withDefault = undefined as any;
+    vm.r$.$value.withCatch = 0 as any;
+    vm.r$.$value.withTransform = 'foo';
     await vm.$nextTick();
 
     expect(vm.r$.$value).toStrictEqual({
+      noValidationValue: 'foo',
       noChange: 'foo',
       withDefault: 'default value',
       withCatch: 'catch',
