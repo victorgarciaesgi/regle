715a6bc248db99cc8549a544ff0ca136bcf8069c
Victor Garcia <victor.garcia@malt.com>
2025-07-09 11:58:14 +0200

    test: fixed config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,28 @@
 import { defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
+import type { AliasOptions } from 'vite';
+import { fileURLToPath } from 'node:url';
+
+const vueVersion = (process.env.VUE_VERSION as '3.4' | '3.5') ?? '3.5';
+
+const alias: AliasOptions = [
+  ...(vueVersion === '3.4'
+    ? [
+        {
+          find: 'vue',
+          replacement: 'vue-3.4',
+        },
+        {
+          find: 'pinia',
+          replacement: 'pinia-2.2.5',
+        },
+      ]
+    : []),
+  {
+    find: /^@regle\/(.*?)$/,
+    replacement: fileURLToPath(new URL('./packages/$1/src', import.meta.url)),
+  },
+];
 
 export default defineConfig({
   test: {
@@ -9,5 +33,49 @@ export default defineConfig({
       include: ['packages/*/src/**'],
       exclude: ['**/*/index.ts', '**/types/**', '**/dist/**/*', '**/*.d.ts', '**/*.spec.ts', 'packages/nuxt/dist/**'],
     },
+    projects: [
+      {
+        plugins: [vue()],
+        extends: true,
+        test: {
+          alias,
+          name: `Global tests - Vue ${vueVersion}.x`,
+          root: './tests',
+          environment: 'happy-dom',
+          include: ['**/*.spec.ts'],
+        },
+      },
+      {
+        plugins: [vue()],
+        extends: true,
+        test: {
+          alias,
+          name: `Rules tests - Vue ${vueVersion}.x`,
+          root: './packages/rules',
+          environment: 'happy-dom',
+          include: ['**/*.spec.ts'],
+        },
+      },
+      {
+        plugins: [vue()],
+        extends: true,
+        test: {
+          alias,
+          name: `Shared tests - Vue ${vueVersion}.x`,
+          root: './packages/shared',
+          environment: 'happy-dom',
+          include: ['**/*.spec.ts'],
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: `Nuxt tests - Vue ${vueVersion}.x`,
+          root: './packages/nuxt',
+          testTimeout: 30000,
+          include: ['**/*.{spec,test}.ts'],
+        },
+      },
+    ],
   },
 });
