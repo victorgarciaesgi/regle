42feed85e1484a0fafca1de3ac91d2aa8f725a23
Victor Garcia <victor.garcia@malt.com>
2025-08-15 13:47:00 +0200

    fix: collections children errors are not working when multiple children have errors #162

--- playground/vue3/src/components/Test12.vue
+++ playground/vue3/src/components/Test12.vue
@@ -1,49 +1,62 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import { useRegle } from '@regle/core';
-import { required, minLength, email, assignIf } from '@regle/rules';
+import { reactive } from 'vue';
+import { useRegleSchema } from '@regle/schemas';
+import * as z from 'zod';
 
-const change = ref(false);
-
-const { r$ } = useRegle(ref({ name: '', email: '' }), {
-  name: assignIf(change, { required, minLength: minLength(4) }),
-  email: { email },
+const ContactPersonSchema = z.object({
+  name: z.string().min(1, 'Name Required'),
+  email: z.email('Error in email'),
+  phone: z
+    .string()
+    .min(1, 'Required')
+    .regex(/^(\+?\d{1,4}[\s-]?)?(\(?\d{1,4}\)?[\s-]?)?[\d\s-]{3,}$/, 'Not Valid'),
 });
 
-async function submit() {
-  const { valid, data } = await r$.$validate();
-  if (valid) {
-    console.log(data.name);
-    //               ^ string
-    console.log(data.email);
-    //.              ^ string | undefined
-  } else {
-    console.warn('Errors: ', r$.$errors);
-  }
-}
+const Schema = z.object({
+  contactPersons: z.array(ContactPersonSchema),
+});
+
+type Dto = z.infer<typeof Schema>;
+
+const state = reactive<Dto>({
+  contactPersons: [
+    {
+      name: '',
+      email: '',
+      phone: '',
+    },
+  ],
+});
+const { r$ } = useRegleSchema(state, Schema);
+
+const addContact = () => {
+  state.contactPersons.push({
+    name: '',
+    email: '',
+    phone: '',
+  });
+};
 </script>
 
 <template>
   <h2>Hello Regle!</h2>
-
-  <input type="checkbox" v-model="change" /> Change <br />
-  <label>Name</label><br />
-  <input v-model="r$.$value.name" placeholder="Type your name" />
-  <ul style="font-size: 12px; color: red">
-    <li v-for="error of r$.$errors.name" :key="error">
-      {{ error }}
-    </li>
-  </ul>
-
-  <label>Email (optional)</label><br />
-  <input v-model="r$.$value.email" placeholder="Type your email" />
-  <ul style="font-size: 12px; color: red">
-    <li v-for="error of r$.$errors.email" :key="error">
-      {{ error }}
-    </li>
-  </ul>
-
-  <button @click="submit">Submit</button>
-  <button @click="r$.$reset({ toInitialState: true })">Restart</button>
+  <div>
+    <label>Errors in the model:</label>
+    <span>{{ r$.contactPersons.$each?.[0].$errors }}</span>
+  </div>
+  <div v-for="(contact, index) in state.contactPersons" :key="index">
+    <div>
+      <label>Person</label>
+      <input v-model="contact.name" />
+    </div>
+    <div>
+      <label>Email</label>
+      <input v-model="contact.email" />
+    </div>
+    <div>
+      <label>Phone</label>
+      <input v-model="contact.phone" />
+    </div>
+  </div>
   <code class="status"> Form status {{ r$.$correct ? '✅' : '❌' }}</code>
 </template>
