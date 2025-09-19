5466207b39a6ed47caa3c67565f42ffd5d208194
Victor Garcia <victor.garcia@malt.com>
2025-09-19 17:34:03 +0200

    chore: updated tests and added docs

--- tests/unit/useRegle/refine-rules/InferInput.spec-d.ts
+++ tests/unit/useRegle/refine-rules/InferInput.spec-d.ts
@@ -1,4 +1,4 @@
-import { refineRules, type InferInput, type MaybeInput } from '@regle/core';
+import { refineRules, useRules, type InferInput, type MaybeInput } from '@regle/core';
 import { dateBefore, number, numeric, required, string, type } from '@regle/rules';
 import { computed, ref } from 'vue';
 
@@ -31,6 +31,8 @@ describe('InferInput should correctly infer state type from a rules object', () 
     overrideRefine: { type: type<number>() },
   }));
 
+  const schema = useRules(rules);
+
   type ExpectedState = {
     firstName?: unknown;
     lastName?: unknown;
@@ -46,6 +48,7 @@ describe('InferInput should correctly infer state type from a rules object', () 
   expectTypeOf<InferInput<typeof rules>>().toEqualTypeOf<ExpectedState>();
   expectTypeOf<InferInput<typeof refRules>>().toEqualTypeOf<ExpectedState>();
   expectTypeOf<InferInput<typeof computedRules>>().toEqualTypeOf<ExpectedState>();
+  expectTypeOf<InferInput<typeof schema>>().toEqualTypeOf<ExpectedState>();
   expectTypeOf<InferInput<typeof refinedRules>>().toExtend<
     ExpectedState & {
       overrideRefine?: MaybeInput<number>;
