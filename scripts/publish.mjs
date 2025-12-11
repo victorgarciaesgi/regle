72abea331b7cbb3587b5d97b26907ce38fb0d5c7
Victor Garcia <victor.garcia@malt.com>
2025-12-11 16:42:23 +0100

    chore: change script publish

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -4,15 +4,13 @@ import { prerelease } from 'semver';
 import { $ } from 'zx';
 const { version } = require('../package.json');
 
-if (process.env.NODE_AUTH_TOKEN) {
-  const tags = prerelease(version);
-  try {
-    if (tags?.length) {
-      await $`pnpm -r --filter='@regle/*' publish --report-summary --no-git-checks --tag ${tags[0]}`;
-    } else {
-      await $`pnpm -r --filter='@regle/*' publish --report-summary --no-git-checks`;
-    }
-  } catch (e) {
-    console.error(e);
+const tags = prerelease(version);
+try {
+  if (tags?.length) {
+    await $`pnpm -r --filter='@regle/*' publish --report-summary --no-git-checks --tag ${tags[0]}`;
+  } else {
+    await $`pnpm -r --filter='@regle/*' publish --report-summary --no-git-checks`;
   }
+} catch (e) {
+  console.error(e);
 }
