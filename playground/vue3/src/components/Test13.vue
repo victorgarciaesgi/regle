7798052e23c75c405b86f37199001f2ddfb0af14
Victor Garcia <victor.garcia@malt.com>
2025-12-20 16:32:58 +0100

    chore: reproduction for #217

--- playground/vue3/src/components/Test13.vue
+++ playground/vue3/src/components/Test13.vue
@@ -1,20 +1,23 @@
 <template>
-  <!-- Values -->
-  <pre>{{ r$.$value }}</pre>
-  <br />
-  <!-- Errors -->
-  <pre>{{ r$.$errors }}</pre>
-
   <div style="display: flex; flex-direction: column; gap: 16px; width: 500px">
     <input v-model="r$.$value.name" />
 
     <!-- Array field -->
     <div style="border: 1px solid; padding: 14px" v-for="field in r$.array.$each">
       <input type="text" :key="field.$id" v-model="field.$value.test" />
-
+      <ul v-if="field.test.$errors.length">
+        <li v-for="error of field.$errors.test" :key="error">
+          {{ error }}
+        </li>
+      </ul>
       <!-- Nested array field -->
       <div style="border: 1px solid; padding: 14px; margin-top: 15px" v-for="nested_field in field.nested_array.$each">
         <input type="text" :key="nested_field.$id" v-model="nested_field.$value.rest" />
+        <ul v-if="nested_field.rest.$errors.length">
+          <li v-for="error of nested_field.$errors.rest" :key="error">
+            {{ error }}
+          </li>
+        </ul>
       </div>
     </div>
 
@@ -30,7 +33,7 @@ import { z } from 'zod';
 
 const values = {
   name: 'test',
-  array: [{ test: 'test', nested_array: [{ rest: 'rest' }] }],
+  array: [{ test: '', nested_array: [{ rest: '' }] }],
 };
 
 const { r$ } = useRegleSchema(
@@ -47,7 +50,10 @@ const { r$ } = useRegleSchema(
         ),
       })
     ),
-  })
+  }),
+  {
+    silent: true,
+  }
 );
 
 const submit = async () => {
